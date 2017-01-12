3aa6dd5da7b6c5e03c9da3a0910845c7e7c4699e
Kristy Miller <miller.kristy06@gmail.com>
2017-01-11 22:39:41 -0600

    Init setup Webpack, Babel, React, Eslint, Travis

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+const App = ({}) => (
+  <div>
+    <h1>App</h1>
+  </div>
+);
+
+render(<App />, document.getElementById('app'));
